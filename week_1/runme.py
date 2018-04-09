d31f98120d1b44f117da0eeee6a648cdcd965805
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2018-04-09 13:10:38 +0200

    variadic plotting for polynomial regression

--- week_1/runme.py
+++ week_1/runme.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 ## basic plot labels
-plt.title('Traffic per Hour')
-plt.ylabel('Traffic')
-plt.xlabel('Hours')
+def addLabels():
+    plt.title('Traffic per Hour')
+    plt.ylabel('Traffic')
+    plt.xlabel('Hours')
 
 ## read file into numpy
 file = './traffic_per_hour.csv'
@@ -16,15 +17,20 @@ arr = np.fromfile(file, dtype='float', sep='\t').reshape(-1, 2)
 arr = arr[~np.isnan(arr).any(axis=1)]
 
 ## ex 1: scatter to plot
-# plt.scatter(arr[:,0], arr[:,1])
-# plt.savefig('plots/traffic_per_hour.png')
+plt.scatter(arr[:,0], arr[:,1])
+addLabels()
+plt.savefig('plots/traffic_per_hour.png')
+plt.close()
 
+## ex2: fitting
 
-## fit linear
-fit = np.polyfit(arr[:,0], arr[:,1], deg=1)
-fit_fn = np.poly1d(fit)
-
-#plt.plot(arr[:,0], arr[:,1], 'yo', arr[:,0], fit_fn(arr[:,0]), '--k')
-#plt.savefig('plots/linear_polyfit.png')
+## change the degree variable to plot different polynoms (linear, quadratic, cubic..)
+for degree in range(1,5):
+    fit = np.polyfit(arr[:,0], arr[:,1], deg=degree)
+    fit_fn = np.poly1d(fit)
+    plt.plot(arr[:,0], arr[:,1], 'yo', arr[:,0], fit_fn(arr[:,0]), '--k')
+    addLabels()
+    plt.savefig('plots/polyfit_degree_' + str(degree) + '.png')
+    plt.close()
 
 
