68416d9901df33e1d82ee40711320693e62da580
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2018-04-09 12:50:48 +0200

    plot

--- week_1/runme.py
+++ week_1/runme.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 file = './traffic_per_hour.csv'
-arr = np.array(np.fromfile(file, dtype='float', sep='\t').reshape(-1, 2))
+arr = np.fromfile(file, dtype='float', sep='\t').reshape(-1, 2)
 arr = arr[~np.isnan(arr).any(axis=1)]
 print(arr)
 
-#plt.plot([1,2,3,4])
-#plt.ylabel('some numbers')
-#plt.show()
+plt.scatter(arr[:,0], arr[:,1])
+plt.title('Traffic per Hour')
+plt.ylabel('Traffic')
+plt.xlabel('Hours')
+plt.show()
