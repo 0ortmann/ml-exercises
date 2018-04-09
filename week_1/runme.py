db387bba787f5be33dd2e12c8cab703ef79af0bf
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2018-04-09 12:44:04 +0200

    remove nan values

--- week_1/runme.py
+++ week_1/runme.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 file = './traffic_per_hour.csv'
-arr = np.fromfile(file, dtype='float', sep='\t')
+arr = np.array(np.fromfile(file, dtype='float', sep='\t').reshape(-1, 2))
+arr = arr[~np.isnan(arr).any(axis=1)]
 print(arr)
 
 #plt.plot([1,2,3,4])
