600a01e24debcc454adc97c37ef0459cb4fdc539
Inum <ina@betaeg.co>
2018-04-19 00:04:10 +0200

    part 4 of assignment 2

--- week_2/ml_exercise02.py
+++ week_2/ml_exercise02.py
@@ -7,13 +7,14 @@ Created on Mon Apr 16 12:25:39 2018
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import math
 
 #exercise 2.1 - numpy operations
 a = np.arange(10)
     
 b = np.full((3,3), True, dtype=bool)
 
-c = a[a%2>0]
+c = a[a%2>0] 
 
 d = np.where(a % 2 == 0, -1, a)
 
@@ -122,4 +123,63 @@ What is special about this matrices?
       of the hilbertmatrix * solution-vector - target vector. Thus any differences to 0 can 
       be considered as error of the numpy library.
       
+'''
+#loss functions
+def l_1(y_true, y_pred):
+    return int(y_true == y_pred)
+# l2_loss. This loss does not appear to work well for values > 1   
+def l_2(y_true, y_pred):
+    return (y_true-y_pred)**2
+    
+def loss_absolute(y_true, y_pred):
+    return abs(y_true-y_pred)
+    
+#distance functions
+def manhattan_distance(x, y):
+    return abs(x['latitude']-y['latitude']) + abs(x['longitude']-y['longitude'])
+
+def euclid_distance(x,y):
+    delta_lat = abs(x['latitude']-y['latitude'])
+    delta_lon = abs(x['longitude']-y['longitude'])
+    return math.sqrt(delta_lat**2 + delta_lon**2)
+
+#the kNN Algorithm
+# returns the predicted_housing_value and the l2_loss with which the 
+# predicted value differs from the exact value
+def kNN(dataset, point, k):    
+    k_nearest = getKNearest(dataset, point, k)
+    predicted_housing_value = sum([x['median_house_value'] for x in k_nearest]) / k
+    # uncomment the next line to use square l2_loss    
+    # loss = l_2(point['median_house_value'], predicted_housing_value)
+    
+    # we decided to use the absolute difference between the housing values, because that metric seems to work better
+    loss = loss_absolute(point['median_house_value'], predicted_housing_value)
+    return predicted_housing_value, loss
+
+# returns the k nearest neighbors of point k in dataset
+def getKNearest(dataset, point, k, distance_function=euclid_distance):
+    distance_point_map = dict()
+    for index, row in dataset.iterrows():
+        dist = distance_function(row, point)
+        distance_point_map[dist] = row
+    indices = sorted(distance_point_map.keys())[:k]
+    return [distance_point_map[i]  for i in indices]
+
+for k in [3,5,7,20]:
+    print "calculating kNN for k", k
+    # note: the following takes some time
+    for index, point in test.iterrows():
+        knn = kNN(train, point, k)
+        print "Checking point: {},{}  : predicted value {}, l2_loss {}".format(point['latitude'], point['longitude'], knn[0], knn[1])
+
+'''
+What are your training and test errors?
+
+    The errors (when considering absolute values) are quite low.
+    The usual housing values are around 150k-400k. 
+    The absolute errors are around 10-150k for k = 3.
+    That is quite a huge error, but it seems reasonable because k is low.
+    For higher k the absolute error gets better.
+    
+    Only when using the l2_loss function (which squares the values), the loss appears to be unrealistically high.
 '''
