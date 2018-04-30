d80cd11a8b1a3dd40dc406ba31288e7cc8fed04e
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2018-04-30 12:10:36 +0200

    ex 4 assignment 1c

--- week_4/ml_exercise04.py
+++ week_4/ml_exercise04.py
@@ -36,3 +36,13 @@ print('Probability to live on the country side:', prob_country_side)
 prob_older_sib = (vaccination_data['olderSiblings'] != 0).sum() / len(vaccination_data['olderSiblings'])
 print('Probability to have at least one older sibling:', prob_older_sib)
 
+## c) more marginal probs
+
+prob_taller_1m = (vaccination_data['height'] > 100).sum() / len(vaccination_data['height'])
+print('Probability to be taller than 1m:', prob_taller_1m)
+
+prob_weight_40kg = (vaccination_data['weight'] > 40).sum() / len(vaccination_data['weight'])
+print('Probability to weigh more than 40kg:', prob_weight_40kg)
+
+prob_had_Y_Z = ((vaccination_data['diseaseY'] == 1).sum() + (vaccination_data['diseaseZ'] == 1).sum()) / len(vaccination_data['weight'])
+print('Probability to have had diseaseY or diseaseZ:', prob_had_Y_Z)
