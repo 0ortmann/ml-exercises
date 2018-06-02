90d40a6a9b8313f32189c6ba11a74552085a2796
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2018-06-02 19:54:18 +0200

    assignment 7: ex 3 a+b

--- week_7/ml_exercise07.py
+++ week_7/ml_exercise07.py
@@ -4,3 +4,43 @@ print('''assignment 7.02
     a) If a single eigenvector covered 0.9 of the data, that would mean that the longest possible, maximized variance describes most of the data very well. The variance in all other dimensions does not describe the data well (0.1). If one would now use the first principle component to reconstruct the original data, one would get out 90 percent, losing 10 percent in other dimensions. 90 percent of all data points are closely situated next to this eigenvector (closer than to any other eigenvector).
     b) If (0,1,1,1) was an eigenvector with 0.85 cover on the data: along the three dimensions of time spent with gaming, facebook and sports is the most information covered (the most spread / variance), but not with age.''')
 
+
+print('''assignment 7.03
+    a) Downloaded script from https://scikit-learn.org/0.15/_downloads/face_recognition.py
+       We had to change line 108, class_weight parameter in call to SVC. We set the parameter to "balanced", before it was "auto", but that is no longer an option with current versions of sklearn.
+    b) The script calls the built-in function "fetch_lfw_people" from sklearn. It downloads a built-in dataset, called "labeled faces in the wild". Properties (taken from the documentation: https://scikit-learn.org/stable/modules/generated/sklearn.datasets.fetch_lfw_people.html)
+        - pictures centered on single face
+        - pixel in rgb
+        - pixel value range: 0.0 - 1.0 (encoded as floats)
+        - image size 62 x 47 pixels
+        - n_samples: 1288
+        - n_features: 1850
+        - n_classes: 7''')
+
+## plot some images of each of the 7 classes (== plot some photos of each person)
+
+from sklearn.datasets import fetch_lfw_people
+from collections import defaultdict
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
+
+images = lfw_people.images
+n_samples, h, w = lfw_people.images.shape
+y = lfw_people.target
+target_names = lfw_people.target_names
+
+person_pics = defaultdict(list)
+for index, person in enumerate(y):
+    person_pics[person] += [index]
+
+for person in person_pics:
+    ## plot first 3 images per person
+    for i in range(3):
+        pic = person_pics[person][i]
+        title = target_names[person] + '_' + str(i)
+        plt.figure(title)
+        plt.imshow(images[pic].reshape((h, w)), cmap=plt.cm.gray)
+        plt.savefig('plots/' + title + '.png')
