d7cc9dccc66c33099349122e9116a5ffcdf935c8
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2018-06-02 18:55:05 +0200

    assignment 7: ex 2

---
+++ week_7/ml_exercise07.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+print('''assignment 7.02
+    a) If a single eigenvector covered 0.9 of the data, that would mean that the longest possible, maximized variance describes most of the data very well. The variance in all other dimensions does not describe the data well (0.1). If one would now use the first principle component to reconstruct the original data, one would get out 90 percent, losing 10 percent in other dimensions. 90 percent of all data points are closely situated next to this eigenvector (closer than to any other eigenvector).
+    b) If (0,1,1,1) was an eigenvector with 0.85 cover on the data: along the three dimensions of time spent with gaming, facebook and sports is the most information covered (the most spread / variance), but not with age.''')
+
