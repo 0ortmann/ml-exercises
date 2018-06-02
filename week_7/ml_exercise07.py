60beb13d42d5416de12143533af4fb6e7861e2d2
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2018-06-02 20:30:16 +0200

    assignment 7: ex 3 e)

--- week_7/ml_exercise07.py
+++ week_7/ml_exercise07.py
@@ -2,7 +2,8 @@
 
 print('''assignment 7.02
     a) If a single eigenvector covered 0.9 of the data, that would mean that the longest possible, maximized variance describes most of the data very well. The variance in all other dimensions does not describe the data well (0.1). If one would now use the first principle component to reconstruct the original data, one would get out 90 percent, losing 10 percent in other dimensions. 90 percent of all data points are closely situated next to this eigenvector (closer than to any other eigenvector).
-    b) If (0,1,1,1) was an eigenvector with 0.85 cover on the data: along the three dimensions of time spent with gaming, facebook and sports is the most information covered (the most spread / variance), but not with age.''')
+    b) If (0,1,1,1) was an eigenvector with 0.85 cover on the data: along the three dimensions of time spent with gaming, facebook and sports is the most information covered (the most spread / variance), but not with age.
+    ''')
 
 
 print('''assignment 7.03
@@ -15,7 +16,9 @@ print('''assignment 7.03
         - image size 62 x 47 pixels
         - n_samples: 1288
         - n_features: 1850
-        - n_classes: 7''')
+        - n_classes: 7
+
+       Plot some images for each class (person)... (see the ./plots folder)''')
 
 ## plot some images of each of the 7 classes (== plot some photos of each person)
 
@@ -44,3 +47,11 @@ for person in person_pics:
         plt.figure(title)
         plt.imshow(images[pic].reshape((h, w)), cmap=plt.cm.gray)
         plt.savefig('plots/' + title + '.png')
+        plt.close()
+
+
+print('''e) Eigenface algorithm in automatic passport control?
+    I don't think it is a good idea to use the (raw) eigenface algorithm in passport control. The idea of the algorithm is to use PCA to decompose face images into a smaller set of images with very charactereristic features. The recognition is done by projecting the test-face to all those eigenfaces and check the distance to each of them.
+    This algorithm may be very useful given a large training data set. But in the passport there is only one foto and one face to test against (the real person). There is not enough data in the passport fotograph to successfully train eg. a SVM.
+    In general, the eigenface algorithm is very robust against illumination or face orientation. But it still requires training data, which is not given for the passports with single fotos that we have nowadays.
+    ''')
