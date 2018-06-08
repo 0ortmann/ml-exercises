d8ff5e9f370e6fc3f9f0155cfbd897f59317adb7
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2018-06-08 11:23:04 +0200

    assignment 8: ex 1 b)

--- week_8/ml_exercise08.py
+++ week_8/ml_exercise08.py
@@ -11,3 +11,13 @@ print('''1 a) different implementations
         IA: holds a count of all non-zero values per row plus all previously seens non-zero values (length = M)
         JA: column index of each element in A (length = K)
     ''')
+
+print('''1 b) operation performance
+    Inserting elements is best with DOK, LIL and COO.
+    - DOK: dict of keys: good for incremental construction + O(1) access
+    - LIL: list of lists: row-based linked list. Good for construction, but insertion could take O(N) -> insert sorted elements.
+    - COO: coordinate format (list of row-col-value tuples): good for insertion, keep values sorted in list (by row, then col). Good to transform to CSR/CSC
+    Matrix Operations (Vector dot product, matrix multiplication) is best with CSR + CSC:
+    - CSR: fast row slicing, slow col slicing. Efficient for traversal -> dot product / multiplicaiton
+    - CSC: fast col slicing, slow row slicing. Efficient for traversal -> dot product / multiplicaiton
+    ''')
