fbbf95068d905d2b99be32fc1e7aa5250e46d2b8
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2018-06-10 17:21:02 +0200

    assignment 8: ex 1 e)

--- week_8/ml_exercise08.py
+++ week_8/ml_exercise08.py
@@ -25,6 +25,7 @@ print('''1 b) operation performance
 
 import numpy as np
 from scipy.sparse import csr_matrix, diags
+from scipy.sparse.linalg import spsolve
 
 print('''1 c) calculate sparsity
     The scipy baseclass for sparse matrices offers two functions 'get_shape()' and 'getnnz()', which can be used to calculate the sparsity of a matrix:
@@ -35,10 +36,39 @@ shape, nnz = A.get_shape(), A.getnnz()
 print('Original matrix data:\n{}\nCSR Matrix:\n{}\nshape: {}\nnon-zero-values: {}'.format(orig_arr, A, shape, nnz))
 print('Sparsity = {}\n'.format(nnz / (shape[0] * shape[1])))
 
+
+## 1 d)
+def rand_diag(n):
+    diagonals = np.random.randint(1, n, size=(3, n))
+    ## use csr format -> better for dot product in 1e)
+    return diags(diagonals, [-1, 0, 1], shape=(n, n), format='csr')
+
 print('''1 d) random sparse tri-diagonal matrix
     Example matrix with n=10:''')
-
 n = 10
-diagonals = np.random.randint(1, n, size=(3, n))
-A = diags(diagonals, [-1, 0, 1], shape=(n, n))
+A = rand_diag(n)
 print(A.toarray())
+
+print('''1 e) solve linear equations:
+    ''')
+
+import time
+
+print('Sparse matrix solving:')
+for n in [10, 100, 1000, 10000, 100000, 1000000, 10000000]:
+    start = time.time()
+    A = rand_diag(n)
+    b = np.ones(n)
+    spsolve(A, b)
+    print('n={} --> {} seconds'.format(n, time.time()-start))
+print('With n=10000000 I used more than 7G ram, this was the maximum I could solve on my machine. But it took only ~9.2 seconds.\n')
+
+print('Dense matrix solving:')
+for n in [10, 100, 1000, 10000]:
+    start = time.time()
+    A = rand_diag(n)
+    b = np.ones(n)
+    np.linalg.solve(A.todense(), b)
+    print('n={} --> {} seconds'.format(n, time.time()-start))
+
+print('''It took more than 236 seconds (~4 minutes) to solve the dense matrix for n=10000 on my machine. For n=100000 I got a memory error when calling the '.todense()' function. The optimizations that can safely be made for sparse matrices have a huge impact on runtime.''')
